e1be7fba20958955079975276bc9455f7c6803dd
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-05-06 18:34:53 +0530

    feat(structs): add initial structs

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ether-wallet-go
+
+go 1.18
---
+++ main_test.go
@@ -0,0 +1,8 @@
+package main
+
+import "testing"
+
+// test if main prints start message
+func TestMain(t *testing.T) {
+	t.Log("⏳  Starting Your Ether Wallet...")
+}
