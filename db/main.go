e9304889c998733891a374443c4a0ca46d5d3f82
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-05-08 18:15:21 +0530

    🔁 change to private

--- db/main.go
+++ db/main.go
@@ -9,7 +9,7 @@ var Db *badger.DB
 func OpenDBConnection() error {
 	// Open the Badger database located in the root of the project directory.
 	// It will be created if it doesn't exist.
-	opts := badger.DefaultOptions("../local/db")
+	opts := badger.DefaultOptions("../private")
 	opts.IndexCacheSize = 100 << 20 // 100 mb or some other size based on the amount of data
 	db, err := badger.Open(opts)
 	if err != nil {
