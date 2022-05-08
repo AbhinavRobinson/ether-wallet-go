4fcdaaf29e39ef6730e91b136ea52b8ab2dec946
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-05-08 18:32:33 +0530

    🚧 using setup for db tests

--- db/main_test.go
+++ db/main_test.go
@@ -2,28 +2,29 @@ package db
 
 import "testing"
 
-// test if connection opens and closes.
-func TestOpenDBConnection(t *testing.T) {
+func setup(tb testing.TB) func(tb testing.TB) {
 	err := OpenDBConnection()
 	if err != nil {
 		panic(err)
 	}
-	err = CloseDBConnection()
-	if err != nil {
-		panic(err)
+	// Return a function to teardown the test
+	return func(tb testing.TB) {
+		defer CloseDBConnection()
 	}
 }
 
+// test if connection opens and closes.
+func TestOpenDBConnection(t *testing.T) {
+	teardown := setup(t)
+	teardown(t)
+}
+
+// test if data is dropped.
 func TestClearDBData(t *testing.T) {
-	err := OpenDBConnection()
-	if err != nil {
-		panic(err)
-	}
-	err = DropDBData()
-	if err != nil {
-		panic(err)
-	}
-	err = CloseDBConnection()
+	teardown := setup(t)
+	defer teardown(t)
+
+	err := DropDBData()
 	if err != nil {
 		panic(err)
 	}
