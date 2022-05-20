fb8b82a20e30bbc9170e1c1ba12522c8859df792
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-05-20 21:59:31 +0530

    📝 feat: add atomic db txns

--- db/main.go
+++ db/main.go
@@ -6,11 +6,13 @@ import (
 
 var Db *badger.DB
 
+// Global Functions
+
 func OpenDBConnection() error {
 	// Open the Badger database located in the root of the project directory.
 	// It will be created if it doesn't exist.
 	opts := badger.DefaultOptions("../private")
-	opts.IndexCacheSize = 100 << 20 // 100 mb or some other size based on the amount of data
+	opts.IndexCacheSize = 100 << 20 // 100 mb, based on the amount of data
 	db, err := badger.Open(opts)
 	if err != nil {
 		return err
@@ -25,4 +27,30 @@ func CloseDBConnection() error {
 
 func DropDBData() error {
 	return Db.DropAll()
+}
+
+// Atomic Functions
+
+func WriteOnDB(key string, value string) error {
+	return Db.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte(key), []byte(value))
+	})
+}
+
+func GetFromDB(key string) (string, error) {
+	var result string
+	err := Db.View(func(txn *badger.Txn) error {
+		value, err := txn.Get([]byte(key))
+		if err != nil {
+			result = value.String()
+		}
+		return err
+	})
+	return result, err
+}
+
+func EraseFromDB(key string) error {
+	return Db.Update(func(txn *badger.Txn) error {
+		return txn.Delete([]byte(key))
+	})
 }
